153d1f2caf859187052ad0484835f5486ac3d209
Fernando Martinez <martinezfer@gmail.com>
2019-01-24 21:23:38 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,3 +1,10 @@
-for (let i = 0; i < 25; i++) {
-  // ...
+ffunction forLoop(array){
+for (i = 0; i < 25; i++) {
+if(i===1){
+array.push(“I am 1 strange loop.”);
+} else {
+array.push(“I am ${i} strange loop.”)
+}
+}
+return array;
 }
